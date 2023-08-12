1854688bdbc3ca69bf612aaee9cecda3e13f2830
Dima <aldmikon@gmail.com>
2023-08-12 15:28:08 +0800

    2.  - Creating .gitignore  - Adding streaming downloads (stream=True agrument of response.get method)  - Adding a progress bar (installing tqdm lib)

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import requests
-from .saving import save_in_file
+from saving import save_in_file
 
 
 def main():
-    url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD"
-    query_parameters = {"downloadformat": "csv"}
+    users_url = "https://databank.worldbank.org/data/download/WDI_CSV.zip"
+    #query_parameters = {"downloadformat": "csv"}
     
     print('Downloading file ...')
-    response = get_response(url,query_parameters)
+    response =  get_response(url=users_url)
     if check_response(response):
-        save_as = "Desktop/gdp_by_country2.zip"
+        save_as = "WDI_CSV.zip"
         save_in_file(save_as,response)
 
 
-
-def get_response(url, query_parameters):
-    return requests.get(url, query_parameters)
+def get_response(url):
+    return requests.get(url,stream=True)
 
 def check_response(response):
     if response.status_code == 200:
-        print('Downloaded successfully!')
         return True
     else:
         print(f'Failed to download the file. Status code: {response.status_code}')
-        return False
     
 main()
 
--- saving.py
+++ saving.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 
 def extract_file_name(save_as):
     file_name,extention = os.path.splitext(os.path.basename(save_as))
@@ -6,9 +7,15 @@ def extract_file_name(save_as):
 
 def save_in_file(save_as,response):
     try:
-        with open(save_as,mode='wb') as file:
-            file.write(response.content)
-            file_name = extract_file_name(save_as)
-            print(f'Saved as {file_name} at {save_as}')
+        with open(save_as,mode='wb') as file,tqdm (
+            desc=save_as, total=int(response.headers.get('content-length', 0)), 
+            unit='B', unit_scale=True
+    ) as bar:
+            for chunk in response.iter_content(chunk_size=10 * 1024):
+                bar.update(len(chunk))
+                file.write(chunk)
+        print('Downloaded successfully!')       
+        file_name = extract_file_name(save_as)
+        print(f'Saved as {file_name} at {save_as}')
     except IOError as e:
         print(f'Error saving the file: {e}')
