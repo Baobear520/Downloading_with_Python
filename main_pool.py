b9288691a9cb824db68810cd3e7624b367ed2f64
Dima <aldmikon@gmail.com>
2023-08-18 16:08:27 +0800

    4.  - Deleting url_templates  - Refactoring main_pool.py  (adding main, extract_file_name functions, adding logging)

--- main_pool.py
+++ main_pool.py
@@ -1,43 +1,55 @@
 from concurrent.futures import ThreadPoolExecutor
-import requests, logging
-from url_template import urls
 from tqdm import tqdm
-
+import requests, logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def download_file(url):
-    try:
-        response = requests.get(url)
-        total_length = int(response.headers.get('content-length', 0))
+# Main function to download these files concurrently using multiple threads of execution
+def main(urls: list):
+    logging.info(f'Starting...')
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_file, urls)
+    logging.info(f'All files have been saved!')
 
+
+def extract_file_names(url):
+    with requests.get(url, stream=True) as response:
         if "content-disposition" in response.headers:
             content_disposition = response.headers["content-disposition"]
             filename = content_disposition.split("filename=")[1]
         else:
             filename = url.split("/")[-1]
+    return response, filename
 
+
+def download_file(url):
+    response, filename = extract_file_names(url)
+    if "content-disposition" in response.headers:
+        content_disposition = response.headers["content-disposition"]
+        filename = content_disposition.split("filename=")[1]
+    else:
+        filename = url.split("/")[-1]
+    total_length = int(response.headers.get('content-length', 0))
+
+    try:
         with open(filename, mode="wb") as file,tqdm(
             desc=filename, total=total_length,
             unit='B', unit_scale=True
         ) as pbar:
             for chunk in response.iter_content(chunk_size=10 * 1024):
                 pbar.update(len(chunk))
-            file.write(chunk)
-        logging.info(f"Downloaded file {filename}")
+                file.write(chunk)
+        logging.info(f"Downloaded {filename} successfully!")
 
     except requests.exceptions.RequestException as e:
         logger.error(f"Failed to download {url}: {e}")
     except Exception as e:
         logger.exception(f"An error occurred while downloading {url}: {e}")
+    
 
-# Main function to download these files concurrently using multiple threads of execution
-def main():
-    with ThreadPoolExecutor() as executor:
-        executor.map(download_file, urls)
 
 if __name__ == "__main__":
     main()
